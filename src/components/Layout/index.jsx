be277928bbbf0910f0c3744bebddd287aa4bafc3
Dhanidayo <sarahjospeh@ymail.com>
2021-08-12 18:42:49 +0100

    removed unused states

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,24 +2,7 @@ import { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 
 function Layout({ children }) {
-  const [wid, setWid] = useState("0%");
-  const [shrink, setShrink] = useState("100%");
-  const [left, setLeft] = useState("10rem");
   const [isNavClosed, setIsNavClosed] = useState(true);
-
-  const openSidenav = () => {
-    setWid("25%");
-    setShrink("55%");
-    setLeft("30rem");
-    // setRight('0rem');
-  };
-
-  const closeSidenav = () => {
-    setWid("0%");
-    setShrink("100%");
-    setLeft("10rem");
-    // setRight('40rem');
-  };
 
   const openNav = () => setIsNavClosed(false);
 
@@ -33,7 +16,7 @@ function Layout({ children }) {
         </button>
       </div>
       <p className="nickname">techbee</p>
-      <Navbar width={wid} closeNav={closeNav} isNavClosed={isNavClosed} />
+      <Navbar closeNav={closeNav} isNavClosed={isNavClosed} />
       <div className="main-content">{children}</div>
     </div>
   );
